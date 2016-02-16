e12aec1747a0b6e3a10904e1ab92afa463b19397
Peter Lollo <peteralollo@gmail.com>
2016-02-15 18:53:08 -0800

    Add bands view page

---
+++ client/app/app.js
@@ -0,0 +1,13 @@
+
+// Rename main module and require more as needed
+angular.module('bands', ['bands.home', 'ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    //Set up Appropriate routes here
+      .when('/home', {
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeController'
+      });
+      //Require and add $httpProvider interceptor here if needed
+  }]);
+  //Add authorization checks here if needed
